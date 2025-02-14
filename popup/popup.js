f7fe071cb29aa3b821c5f1bbe3c8edf1f7d24156
Javier Herrera <javierhb23@outlook.com>
2025-02-14 18:15:56 -0300

    Remove error checking

--- popup/popup.js
+++ popup/popup.js
@@ -1,6 +1,6 @@
 import "./utils.js"
 
-document.addEventListener("DOMContentLoaded", handlePopupLoad);
+document.addEventListener("DOMContentLoaded", handlePopupLoaded);
 document.querySelector("#toggle-extension").addEventListener("change", handleToggleExtension);
 document.querySelectorAll("[name=select-config]").forEach((element) => {
     element.addEventListener("change", handleSelectConfig);
@@ -28,34 +28,20 @@ async function getTab() {
     return tab;
 }
 
-function displayError(error) {
-    const container = document.querySelector("#error-container");
-    const pError = document.querySelector("#error");
-    const btnDismiss = document.querySelector("#btn-dismiss");
-    container.style.display = null;
-    pError.innerHTML = `<span class="fw-bold">${error.name}</span>: ${error.message}`;
-    btnDismiss.addEventListener("click", () => container.style.display = "none");
-}
-
-async function handlePopupLoad(event) {
+async function handlePopupLoaded(event) {
     // TODO: Retrieve saved settings and apply them on page load
     // TODO: Retrieve saved settings into popup inputs
 
-    try {
-        const tab = await getTab();
-        const { sites } = await chrome.storage.local.get(["sites"]);
+    const tab = await getTab();
+    const { sites } = await chrome.storage.local.get(["sites"]);
 
-        if (!sites)
-            chrome.storage.local.set({ sites: [] });
+    if (!sites)
+        chrome.storage.local.set({ sites: [] });
 
-        const urls = sites.map((site) => site.url);
-        for (const url of urls) {
-            const isMatch = new RegExp(url.replace("*", ".*")).test(tab.url);
-            document.querySelector("#url").value = isMatch ? url : tab.url;
-        }
-        // throw new TypeError("my type error");
-    } catch (error) {
-        displayError(error);
+    const urls = sites.map((site) => site.url);
+    for (const url of urls) {
+        const isMatch = new RegExp(url.replace("*", ".*")).test(tab.url);
+        document.querySelector("#url").value = isMatch ? url : tab.url;
     }
 }
 
@@ -69,26 +55,18 @@ async function handleSelectConfig(event) {
 }
 
 async function handleInputChanged(event) {
-    try {
-        const tab = await getTab();
-        message.styles = await getStylesFromPopup();
-        const error = await chrome.tabs.sendMessage(tab.id, message);
-        if (error) p.innerText = `${error}`;
-    } catch (e) {
-        displayError(e)
-    }
+    const tab = await getTab();
+    message.styles = await getStylesFromPopup();
+    const error = await chrome.tabs.sendMessage(tab.id, message);
+    if (error) p.innerText = `${error}`;
 }
 
 async function handleToggleExtension(event) {
     const action = event.target.checked ? "enable" : "disable";
     const message = { action };
 
-    try {
-        const tab = await getTab()
-        const response = await chrome.tabs.sendMessage(tab.id, message);
-        if (response) throw new Error(response);
-    } catch (error) {
-        displayError(error);
-    }
+    const tab = await getTab()
+    const response = await chrome.tabs.sendMessage(tab.id, message);
+    if (response) throw new Error(response);
 }
 
