fff9f9a676e5221e3ffd5fcbee9ea62de402253c
Javier Herrera <javierhb23@outlook.com>
2025-05-13 16:52:01 -0400

    Remove redundant code from content script

--- entrypoints/content.js
+++ entrypoints/content.js
@@ -8,10 +8,6 @@ export default defineContentScript({
             }
 
             if (message.styles) {
-                // Remove existing inline styles
-                for (const prop in document.body.style) {
-                    document.body.style[prop] = null;
-                }
                 // Apply given styles
                 for (const prop in message.styles) {
                     document.body.style[prop] = message.styles[prop] ?? null;
