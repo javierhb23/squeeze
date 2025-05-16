758c6cd6f6410460fa24beff19e996216e607860
Javier Herrera <javierhb23@outlook.com>
2025-05-16 12:16:33 -0400

    Remove redundant ping in content script

--- entrypoints/content.js
+++ entrypoints/content.js
@@ -2,11 +2,6 @@ export default defineContentScript({
     matches: ['<all_urls>'],
     main() {
         chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-            if (message === "ping") {
-                sendResponse("pong");
-                return true;
-            }
-
             if (message.styles) {
                 // Apply given styles
                 for (const prop in message.styles) {
