788a11aa85517b4c319844f405e3db9abb3677de
Javier Herrera <javierhb23@outlook.com>
2025-05-20 18:35:51 -0400

    Create error wrapper function for message handlers

--- entrypoints/background/handlers.js
+++ entrypoints/background/handlers.js
@@ -20,7 +20,26 @@ async function webNavigationHandler({ url, tabId }) {
     applyStyles(url, tabId);
 }
 
-async function info(request) {
+/**
+ * A try-catch wrapper for request handling functions (async only).
+ *
+ * @param {function(): Promise} fn - A request handling function
+ * @returns {function(): Promise}
+ */
+function errorHandler(fn) {
+    return async function (request) {
+        return fn(...arguments).catch(error => {
+            return {
+                request: request,
+                status: "error",
+                errorName: error.name,
+                errorMessage: error.message
+            }
+        });
+    }
+}
+
+const info = errorHandler(async (request) => {
     const tab = await getTab();
     const storage = await chrome.storage.local.get();
     const sites = new SitesStorage(storage.sites);
@@ -33,49 +52,33 @@ async function info(request) {
         storage: storage
     };
     return response;
-}
+});
 
-async function updateStyles(request) {
+const updateStyles = errorHandler(async (request) => {
     const response = { request };
-    try {
-        await chrome.storage.local.set({ globalStyles: request.styles });
-        styleTabs();
-    } catch (error) {
-        response.status = error.name;
-        response.error = error.message;
-    }
+    await chrome.storage.local.set({ globalStyles: request.styles });
+    styleTabs();
     return response;
-}
+});
 
-async function addSite(request) {
+const addSite = errorHandler(async (request) => {
     const storage = await chrome.storage.local.get();
     const sites = new SitesStorage(storage.sites);
-
     const response = { request };
-    try {
-        await sites.add(request.url);
-        styleTabs();
-        response.status = `${request.url} was added to storage successfully`
-    } catch (error) {
-        response.status = error.name;
-        response.error = error.message;
-    }
+    await sites.add(request.url);
+    styleTabs();
+    response.status = `${request.url} was added to storage successfully`
     return response;
-}
+});
 
-async function removeSite(request) {
+const removeSite = errorHandler(async (request) => {
     const response = { request };
     const storage = await chrome.storage.local.get("sites");
     const sites = new SitesStorage(storage.sites);
-    try {
-        await sites.remove(request.url);
-        styleTabs();
-        response.status = `${request.url} was removed successfully`
-    } catch (error) {
-        response.status = error.name;
-        response.error = error.message;
-    }
+    await sites.remove(request.url);
+    styleTabs();
+    response.status = `${request.url} was removed successfully`
     return response;
-}
+});
 
 export { messageHandler, webNavigationHandler };
