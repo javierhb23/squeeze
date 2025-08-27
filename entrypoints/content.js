687320e56dcb5ac234c299dca34da54f5201ffcf
Javier Herrera <javierhb23@outlook.com>
2025-08-27 11:51:12 -0400

    Replace chrome.* call with browser.* in content script

--- entrypoints/content.js
+++ entrypoints/content.js
@@ -1,7 +1,7 @@
 export default defineContentScript({
     matches: ['<all_urls>'],
     main() {
-        chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+        browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
             if (message.styles) {
                 // Apply given styles
                 for (const prop in message.styles) {
